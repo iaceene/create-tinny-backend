1fe6e31aa5ff58b54955bf246bc2340da3fb2c9a
iaceene <yassineajagrou986@gmail.com>
2026-06-28 20:48:34 +0100

    adding the default page

--- template/app/index.ts
+++ template/app/index.ts
@@ -1,6 +1,14 @@
-import Server from "tinny-backend";
+import type { ServerReq, ServerRes } from "tinny-backend";
 import CreateServer from "../lib/CreateSrv.js";
 
 const srv = CreateServer(true, 3000);
+
+srv.add({
+    path: "/",
+    method: "GET",
+    handler: async (req: ServerReq, res: ServerRes)=>{
+        return await srv.SendFile(res, "./public/example/default.html", 200)
+    }
+})
 
 srv.listen()
